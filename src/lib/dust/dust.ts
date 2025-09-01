af3ba7fb0239bc6ab08bbed91458b0255ce7eb80
deronek <dionek3@gmail.com>
2025-09-01 21:50:14 +0200

    Enable immutability for getDustData

--- src/lib/dust/dust.ts
+++ src/lib/dust/dust.ts
@@ -1,4 +1,8 @@
 import raw from "./poe-dust.json";
 import type { Item } from "./schema";
 
-export const getDustData = (): Item[] => raw;
+type DeepReadonly<T> = {
+  readonly [K in keyof T]: T[K] extends object ? DeepReadonly<T[K]> : T[K];
+};
+
+export const getDustData = (): DeepReadonly<Item[]> => raw;
