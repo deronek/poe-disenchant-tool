385862f646b8b601fca93637cb53d823907aba27
deronek <dionek3@gmail.com>
2025-10-14 21:18:38 +0200

    Add cursor-help to tooltip trigger

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -47,9 +47,16 @@ function Tooltip({
 }
 
 function TooltipTrigger({
+  className,
   ...props
 }: React.ComponentProps<typeof TooltipPrimitive.Trigger>) {
-  return <TooltipPrimitive.Trigger data-slot="tooltip-trigger" {...props} />;
+  return (
+    <TooltipPrimitive.Trigger
+      data-slot="tooltip-trigger"
+      className={cn("cursor-help", className)}
+      {...props}
+    />
+  );
 }
 
 function TooltipContent({
