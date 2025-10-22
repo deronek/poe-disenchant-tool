f3d3cb9c408707222a3e2056be849ddf44efdbef
deronek <dionek3@gmail.com>
2025-10-22 13:49:12 +0200

    Clean-up layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={"antialiased"}>
+      <body className="antialiased">
         <TooltipProvider>
           <ThemeProvider
             attribute="class"
