48087acdebd1c7e39a04c66e1e2c8da4c198f8bc
deronek <dionek3@gmail.com>
2025-11-01 21:13:09 +0100

    Add catalyst calculation and info to desktop UI

--- src/lib/itemData/itemData.ts
+++ src/lib/itemData/itemData.ts
@@ -18,6 +18,7 @@ export type Item = {
   dustPerChaosPerSlot: number;
   type: AllowedUnique;
   icon: string;
+  shouldCatalyst: boolean;
 };
 
 const createUniqueId = (name: string, variant?: string) =>
@@ -41,12 +42,11 @@ const uncached__getItems = async (league: League) => {
       continue;
     }
 
-    const calculatedDustValue = calculateDustValue(priceItem, dustItem);
-
-    const dustPerChaos =
-      priceItem.chaos > 0
-        ? Math.round(calculatedDustValue / priceItem.chaos)
-        : 0;
+    const {
+      dustValue: calculatedDustValue,
+      dustPerChaos,
+      catalyst: shouldCatalyst,
+    } = calculateDustEfficiency(priceItem, dustItem);
 
     merged.push({
       id: id++,
@@ -56,11 +56,12 @@ const uncached__getItems = async (league: League) => {
       listingCount: priceItem.listingCount,
       variant: priceItem.baseType,
       calculatedDustValue,
-      dustPerChaos: dustPerChaos,
+      dustPerChaos: Math.round(dustPerChaos),
       slots: dustItem.slots,
       dustPerChaosPerSlot: Math.round(dustPerChaos / dustItem.slots),
       type: priceItem.type,
       icon: priceItem.icon,
+      shouldCatalyst: shouldCatalyst,
     });
   }
 
@@ -73,10 +74,40 @@ const uncached__getItems = async (league: League) => {
   };
 };
 
-function calculateDustValue(priceItem: PriceItem, dustItem: DustItem) {
-  return priceItem.type === "UniqueAccessory"
-    ? dustItem.dustValIlvl84
-    : dustItem.dustValIlvl84Q20;
+function calculateDustEfficiency(priceItem: PriceItem, dustItem: DustItem) {
+  if (priceItem.type !== "UniqueAccessory") {
+    // Weapon or Armor, always cheap to quality up
+    return {
+      dustValue: dustItem.dustValIlvl84Q20,
+      dustPerChaos: dustItem.dustValIlvl84Q20 / priceItem.chaos,
+      catalyst: false,
+    };
+  }
+
+  // For jewelery, calculate if it's worth it to add quality
+
+  // Assume 1c per catalyst
+  const costToAddQuality = 20; // 20 catalysts, 1c each
+
+  const defaultDustPerChaos = dustItem.dustValIlvl84 / priceItem.chaos;
+  const catalystedDustPerChaos =
+    dustItem.dustValIlvl84Q20 / (priceItem.chaos + costToAddQuality);
+
+  if (catalystedDustPerChaos > defaultDustPerChaos) {
+    // Quality up is worth it
+    return {
+      dustValue: dustItem.dustValIlvl84Q20,
+      dustPerChaos: catalystedDustPerChaos,
+      catalyst: true,
+    };
+  } else {
+    // Quality up is not worth it
+    return {
+      dustValue: dustItem.dustValIlvl84,
+      dustPerChaos: defaultDustPerChaos,
+      catalyst: false,
+    };
+  }
 }
 
 /**
@@ -108,6 +139,6 @@ function calculateLowStockThreshold(items: Item[]) {
 export const getItems = async (league: League) => {
   return unstable_cache(async () => uncached__getItems(league), [league], {
     tags: [`items-${league}`],
-    revalidate: 900, // 15 minutes
+    revalidate: 1, // 15 minutes
   })();
 };
