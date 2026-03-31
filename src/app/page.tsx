8a05c7d85683b9c4907fe56d99f7457bc73aa661
deronek <dionek3@gmail.com>
2026-03-31 13:59:50 +0200

    Replace root route redirect with CDN-level redirect

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from "next/navigation";
-
-import { DEFAULT_LEAGUE } from "@/lib/leagues";
-
-export default function Home() {
-  // Redirect to default league
-  redirect(`/${DEFAULT_LEAGUE}`);
-}
